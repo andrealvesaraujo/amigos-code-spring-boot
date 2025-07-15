964887ace9b68fa980b82bd89bed802fbb38e12b
andrealvesaraujo <andrealvesaraujo.1995@gmail.com>
2025-07-15 12:29:39 -0300

    Add my first api in application java

--- src/main/java/com/amigoscode/AmigosCodeSpringBootApplication.java
+++ src/main/java/com/amigoscode/AmigosCodeSpringBootApplication.java
@@ -2,12 +2,20 @@ package com.amigoscode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class AmigosCodeSpringBootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AmigosCodeSpringBootApplication.class, args);
+	}
+
+	@GetMapping
+	public String helloWorld(){
+		return "Hello World Spring Boot";
 	}
 
 }
