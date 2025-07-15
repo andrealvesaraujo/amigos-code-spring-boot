855d3c328cfd1fc8fe050ae14802ec534e2982d8
andrealvesaraujo <andrealvesaraujo.1995@gmail.com>
2025-07-15 12:01:39 -0300

    Initial commit

---
+++ src/main/java/com/amigoscode/AmigosCodeSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.amigoscode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmigosCodeSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmigosCodeSpringBootApplication.class, args);
+	}
+
+}
